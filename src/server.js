03f9fa6220605ee98443b1dd3f29794ca94f00ec
taeyoung <xoxodudwkd@gmail.com>
2019-10-29 14:21:19 +0900

    test socket emit

--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
 import express from 'express';
 import path from 'path';
-import socketID from 'socket.io';
+import socketIO from 'socket.io';
+import logger from 'morgan';
 
 const PORT = 4000;
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => res.render('home'));
 
 const handleListening = () => {
-  console.log(`(～￣▽￣)～ Listening to http://localhost:${PORT}`);
+  console.log(`(～￣▽￣)～ Listening to: http://localhost:${PORT}`);
 };
 
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+io.on('connection', socket => {
+  socket.emit('hello');
+  socket.on('test to server', () => console.log('Server receive msg'));
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,6 @@
-hello;
+// eslint-disable-next-line no-undef
+const socket = io('/');
+
+socket.on('hello', () => console.log('Hello Test'));
+
+socket.emit('test to server');
