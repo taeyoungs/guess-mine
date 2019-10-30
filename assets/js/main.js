fc0d30f9538116f4fe4e364ebf0af39775b8bc5f
taeyoung <xoxodudwkd@gmail.com>
2019-10-30 15:30:46 +0900

    login flow 3/4

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,27 @@ const NICKNAME = 'nickname';
 
 const nickname = localStorage.getItem(NICKNAME);
 
+const logIn = nickname => {
+  const socket = io('/');
+  console.log(nickname);
+  socket.emit('setNickname', { nickname });
+};
+
 if (nickname === null) {
-  body.className = LOGGED_IN;
-} else {
   body.className = LOGGED_OUT;
+} else {
+  body.className = LOGGED_IN;
+  logIn(nickname);
 }
 
 const handleFormSubmit = event => {
   event.preventDefault();
   const input = loginForm.querySelector('input');
   console.log(input.value);
-  input.value = '';
   localStorage.setItem(NICKNAME, input.value);
+  body.className = LOGGED_IN;
+  logIn(input.value);
+  input.value = '';
 };
 
 if (loginForm) {
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import socketIO from 'socket.io';
 import logger from 'morgan';
+import socketController from './socketController';
 
 const PORT = 4000;
 const app = express();
@@ -20,14 +21,4 @@ const handleListening = () => {
 const server = app.listen(PORT, handleListening);
 const io = socketIO.listen(server);
 
-io.on('connection', socket => {
-  socket.on('newMessage', ({ message }) => {
-    socket.broadcast.emit('messageNotif', {
-      message,
-      nickname: socket.nickname || 'Anonymous',
-    });
-  });
-  socket.on('setNickname', ({ nickname }) => {
-    socket.nickname = nickname;
-  });
-});
+io.on('connection', socket => socketController(socket));
